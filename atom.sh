d4bb955f04c02a7fe75b916a3e2e1d206dd16b8a
ichiki <i.tennis0210@gmail.com>
2016-04-13 16:28:22 +0900

    add atom packages

--- atom.sh
+++ atom.sh
@@ -1,7 +1,32 @@
+## Syntax Error Hightlighting
 apm install linter
+apm install linter-jshint # Javascript
+apm install linter-tidy # HTML
+apm install linter-csslint # CSS
+apm install linter-scss-lint # SCSS
+apm install linter-ruby #Ruby
+apm install linter-coffeelint # CoffeeScript
+apm install linter-markdownlint # Markdown
+apm install linter-jsonlint # Json
+
+# Autocomplete
+apm install autocomplete-plus
+apm install atom-ternjs # Javascript
+apm install autocomplete-html # HTML
+apm install autocomplete-css # CSS
+apm install autocomplete-ruby # Ruby
+
+# Modify indent automatically
 apm install atom-beautify
+
+# Change editor to vim
 apm install vim-mode
-apm install file-icons
-apm install git-plus
-apm install jumpy
 apm install ex-mode
+
+# Assign file extension icons
+apm install file-icons
+
+# Create dynamic hotkeys to jump around files
+apm install jumpy
+
+apm install find-and-replace
